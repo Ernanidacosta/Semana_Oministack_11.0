4bc22cd23d27c6c84ef9b04b4b5bae047a59786e
Ernani da Costa <ernanidacosta@gmail.com>
2020-03-27 15:59:41 -0300

    install nodemon package

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/users', (request, response) => {
+    const params = request.query;
+
+    console.log(params);
     
+    return response.json({
+        evento: 'Semana Oministack 11',
+        aluno: 'Ernani da Costa',
+    });    
 });
 
 app.listen(3333);
