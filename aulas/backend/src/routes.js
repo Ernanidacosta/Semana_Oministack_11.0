07bd905d48aa652824098638067061d7f255a32e
Ernani da Costa <ernanidacosta@gmail.com>
2020-03-27 21:13:59 -0300

    some updates

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,15 +1,10 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController');
+
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-    const body = request.body;
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-    console.log(body);
-
-    return response.json({
-        evento: 'Semana Oministack 11',
-        aluno: 'Nito da Costa',
-    });    
-});
 module.exports = routes;
