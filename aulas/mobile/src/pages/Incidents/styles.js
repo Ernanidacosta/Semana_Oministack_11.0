d8b82cba29e6f2de71bdb61101c6190b57a7fb52
Ernani da Costa <ernanidacosta@gmail.com>
2020-04-03 00:57:48 -0300

    Many updates

---
+++ aulas/mobile/src/pages/Incidents/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import {constants} from 'expo-constants'
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        paddingHorizontal: 24,
+        
+    }
+});
