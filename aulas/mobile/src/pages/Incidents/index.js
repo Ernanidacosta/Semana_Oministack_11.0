a2f4b5126b7e894b0ff1c096ff932422995b1eb5
Ernani da Costa <ernanidacosta@gmail.com>
2020-04-03 14:25:02 -0300

    finalized list of cases

--- aulas/mobile/src/pages/Incidents/index.js
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -1,13 +1,19 @@
 import React from "react";
+import {useNavigation} from '@react-navigation/native';
 import { Feather } from "@expo/vector-icons";
-import { View, Image, Text, TouchableOpacity } from "react-native";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
 
 import logoImg from "../../assets/logo.png";
 
 import styles from "./styles";
 
 export default function Incidents() {
-  return (
+    const navigation = useNavigation();
+
+    function navigateToDetail() {
+        navigation.navigate('Detail');
+    }
+    return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImg} />
@@ -20,55 +26,33 @@ export default function Incidents() {
         Escolha um dos casos abaixo e salve o dia.
       </Text>
 
-      <View style={styles.incidentList}>
-        <View style={styles.incident}>
-          <Text style={styles.incidentProperty}>ONG:</Text>
-          <Text style={styles.incidentValue}>AACD</Text>
+      <FlatList
+        data={[1, 2, 3]}
+        style={styles.incidentList}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>AACD</Text>
 
-          <Text style={styles.incidentProperty}>Caso:</Text>
-          <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+            <Text style={styles.incidentProperty}>Caso:</Text>
+            <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
 
-          <Text style={styles.incidentProperty}>Valor:</Text>
-          <Text style={styles.incidentValue}>R$ 150,00</Text>
+            <Text style={styles.incidentProperty}>Valor:</Text>
+            <Text style={styles.incidentValue}>R$ 150,00</Text>
 
-          <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
-            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-            <Feather name="arrow-right" size={16} color="#e02041" />
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.incident}>
-          <Text style={styles.incidentProperty}>ONG:</Text>
-          <Text style={styles.incidentValue}>AACD</Text>
-
-          <Text style={styles.incidentProperty}>Caso:</Text>
-          <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
-
-          <Text style={styles.incidentProperty}>Valor:</Text>
-          <Text style={styles.incidentValue}>R$ 150,00</Text>
-
-          <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
-            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-            <Feather name="arrow-right" size={16} color="#e02041" />
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.incident}>
-          <Text style={styles.incidentProperty}>ONG:</Text>
-          <Text style={styles.incidentValue}>AACD</Text>
-
-          <Text style={styles.incidentProperty}>Caso:</Text>
-          <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
-
-          <Text style={styles.incidentProperty}>Valor:</Text>
-          <Text style={styles.incidentValue}>R$ 150,00</Text>
-
-          <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
-            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-            <Feather name="arrow-right" size={16} color="#e02041" />
-          </TouchableOpacity>
-        </View>
-      </View>
+            <TouchableOpacity 
+                style={styles.detailsButton}
+                onPress={navigateToDetail}
+            >
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
+ 
