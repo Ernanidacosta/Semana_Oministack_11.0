a81c674fa5ec0331d362d4c09d3d4966287297e7
Ernani da Costa <ernanidacosta@gmail.com>
2020-04-08 20:56:58 -0300

    improve test cases

--- aulas/backend/tests/integration/ong.spec.js
+++ aulas/backend/tests/integration/ong.spec.js
@@ -1,7 +1,24 @@
 const request = require('supertest');
+const app = require('../../src/app')
+const connection = require('../../src/database/connection');
+
 
 describe('ONG', ()=>{
-    it('shoud be able to create a new ONG', () => {
+    beforeEach(async() => {
+        await connection.migrate.latest();
+    });
 
+    it('shoud be able to create a new ONG', async () => {
+        const response = await request(app)
+        .post('/ongs')
+        .send({
+            name: "Lauana",
+            email: "contato@mail.com",
+            whatsapp: "5581999998584",
+            city: "recife",
+            uf: "PE"	
+        });
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
     });
 });
